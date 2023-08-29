8d71da756e1fdecc4d7df0cbd105264368a8660b
cedric candes <candaescedric63@gmail.com>
2023-08-29 16:25:33 +0200

    cypress backmarket login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,11 +2,42 @@ describe("go to backmarket", () => {
   it("accept cookies", () => {
     cy.visit("https://preprod.backmarket.fr/fr-fr/register");
     cy.get("button").contains("OK").click();
-    cy.wait(1000)
+    cy.get("form").eq(2).find("input").eq(0).type("Pamela");
+    cy.get("form").eq(2).find("input").eq(1).type("Pamela");
+    cy.get("form")
+      .eq(2)
+      .find("input")
+      .eq(2)
+      .type("Pamelac'estlameileur@gmail.com");
+    cy.get("form").eq(2).find("input").eq(3).type("Pamela1.");
+    cy.get("button").contains("Enchantés ").click();
   });
-  describe("register to backmarket", () => {
-    it("insert registration data"), () => {
-      cy.get('form')
-    };
+  // describe("create account", () => {
+  it("accept cookies", () => {
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+    cy.get("button").contains("OK").click();
+    cy.get("form").eq(2).find("input").eq(0).type("Pamela");
+    cy.get("form").eq(2).find("input").eq(1).type("Pamela");
+    cy.get("form")
+      .eq(2)
+      .find("input")
+      .eq(2)
+      .type("Pamelac'estlameileur@gmail.com");
+    cy.get("form").eq(2).find("input").eq(3).type("Pamela");
+    cy.get("button").contains("Enchantés ").click();
+  });
+  // });
+  // describe("go to backmarket", () => {
+  it("accept cookies", () => {
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+    cy.get("button").contains("OK").click();
+    cy.get("form")
+      .eq(1)
+      .find("input")
+      .eq(0)
+      .type("Pamelac'estlameileur@gmail.com");
+    cy.get("form").eq(1).find("input").eq(1).type("Pamela1.");
+    cy.get("button").contains("Welcome ").click();
   });
 });
+// });
